8f433cbb17f8496f3ab72d0b2da679c9cc215f65
Rubén Robles <me@d8vjork.com>
2023-10-30 19:23:32 +0100

    add more unit tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -4,13 +4,24 @@ import assert from 'assert';
 // as well as import your extension to test it
 import vscode from 'vscode';
 import { namespaceJoin } from '../../util/namespaces';
+import { capitalize, lowercase } from '../../util/misc';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  vscode.window.showInformationMessage('Start all tests.');
 
-	test('namespaceJoin', () => {
+  test('namespaceJoin', () => {
     assert.strictEqual(namespaceJoin('App\\', '\\Support\\', '/MyClass'), 'App\\Support\\MyClass');
     assert.strictEqual(namespaceJoin('App\\', '\\Support\\', '/MyClass.php'), 'App\\Support');
-	});
+  });
+
+  test('lowercase', () => {
+    assert.strictEqual(lowercase('HELLO WORLD'), 'hello world');
+    assert.strictEqual(capitalize('hello world'), 'hello world');
+  });
+
+  test('capitalize', () => {
+    assert.strictEqual(capitalize('my test'), 'My test');
+    assert.strictEqual(capitalize('My test'), 'My test');
+  });
 });
